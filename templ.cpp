8e7290e07a4fcf1f9f16fe0f8636856a66e6a6ed
tokyoster <take.it.easy.ggl@gmail.com>
2013-12-08 13:57:29 +0900

    Create templ.cpp
    プログラミングコンテスト用のc++のスケルトンファイル ver. 0.1

---
+++ templ.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <cstdlib>
+#include <map>
+#include <stack>
+
+#define MAX_N 1000
+#define MAX_M 1000
+#define INF 100000000
+
+using namespace std;
+
+typedef long long ll;
+struct edge {int to, cost;};
+typedef pair<int,int> P;
+
+
+int n;
+int a[MAX_N];
+
+void solve(){
+}
+
+int main(){
+	scanf("%d",&n);
+	for (int i=0;i<n; i++){
+		scanf("%d",&a[i]);
+	}
+	solve();
+	return 0;
+}
